a6197785927d75e500482a62d6b36477fd916c1e
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-05-17 16:47:05 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready',() => {
+	//Edit after game so u can add wut game bot is playing between "" xd
+	   client.user.setPresence({game: {name: "", type: 0}});
+});
+
+var prefix = "m."
+
+client.on('message', message => {
+	if (message.author.bot){
+	if (message.content.startsWith('Bungou Stray')) {
+
+		 message.channel.send(`@everyone WHAPAM );
+	}}
+});
+//Important
+client.login(process.env.BOT_TOKEN);
