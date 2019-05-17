78de4d6c1e17cc6343a864e707b3f4582d21928a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-05-17 17:14:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,30 @@ const client = new Discord.Client();
 var prefix = "y!"
 var xprefix = "Y!"
 
-   
+var hello = [
+ 	"Hi",
+	"Ello uwu",
+	"Ey",
+	"Hellu :3"
+]
+
+
+
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith((prefix)||(xprefix) + 'ping')) {
 	
 		 message.channel.send(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``);
 	}
+});
+  client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith('Ey')||('Hello')||('Hi')||('Ello')) {
+	
+		 var randomAnswer = hello[Math.floor(Math.random() * hello.length)];
+		message.channel.send(randomAnswer);
+	}
+
 });
   
 
