0ed40cc557080267140952b75f85ac0da4a41453
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-19 22:49:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,56 +3,18 @@ const client = new Discord.Client();
 
 
 
-var prefix = "y!"
-var xprefix = "Y!"
-
-
-var helloV = [
- 	"Hi",
-	"Ello,
-	"Ey",
-	"Hello"
-]
-
-var hello = [
- 	"Hi",
-	"Ello uwu",
-	"Ey",
-	"Hellu :3"
-]
-
-
-
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith((prefix || xprefix) + 'ping')) {
-	
-		 message.channel.send(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``);
-	}
-});
-  client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(helloV) {
-	
-		 var randomAnswer = hello[Math.floor(Math.random() * hello.length)];
-		message.channel.send(randomAnswer);
-	}
-
+client.on("ready", () => {
+  const channel = client.channels.get("710730014827479047");
+  if (!channel) return console.error("The channel does not exist!");
+  channel.join().then(connection => {
+    // Yay, it worked!
+    console.log("Successfully connected.");
+  }).catch(e => {
+    // Oh no, it errored! Let's log it to console :)
+    console.error(e);
+  });
 });
 
 
-    client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith('Send nudes') {
-	
-const embed = new Discord.RichEmbed()
-  .setTitle("My nudes")
-  .setColor(0x7401DF)
-  .setImage("https://cdn.discordapp.com/attachments/373987435299733504/579073054471094273/unknown.png")
-  message.channel.send({embed});
-		
-	}
-
-});
 
 client.login(process.env.BOT_TOKEN);
